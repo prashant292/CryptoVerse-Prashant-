a30ed3ab8a2b723f1b1f6f2f86b0df6e9f905d6b
Prashant Pratap Singh <98754654+prashant292@users.noreply.github.com>
2024-01-02 01:22:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {BrowserRouter, Route} from "react-router-dom";
+import './App.css';
+import Header from "./components/Header"
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core"
+import Alert from './components/Alert'
+function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh"
+    }
+  }));
+  const classes = useStyles()
+  
+  return (
+    <BrowserRouter>
+      <div className={classes.App}> 
+            <Header />
+            <Route path = "/" component={Homepage} exact />
+            <Route path = "/coins/:id" component={CoinPage} />
+      </div>
+      <Alert />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/SelectButton.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid skyblue",
+      borderRadius: 5,
+      padding: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "skyblue" : "",
+      color: selected ? "black" : "",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "skyblue",
+        color: "black",
+      },
+      width: "22%",
+      //   margin: 5,
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
